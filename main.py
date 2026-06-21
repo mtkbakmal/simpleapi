29ea6ed864429e22e45ff525a4775856e30ac680
mtkbakmal <matkurb.akmal@icloud.com>
2026-06-21 22:07:25 +0600

    fix

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import uvicorn
 from fastapi import FastAPI, staticfiles
 from app.routers.users import users_router
+from a2wsgi import ASGIMiddleware
     
 app = FastAPI()
 app.include_router(users_router, tags=["Users"])
 app.mount("/", staticfiles.StaticFiles(directory="app/public", html=True), name="/static")
+
+wsgi_app = ASGIMiddleware(app) #type: ignore
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True)
