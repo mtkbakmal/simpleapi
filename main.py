81cc4ce9aef7e1cf805a95622284bddfc6cb24e2
mtkbakmal <matkurb.akmal@icloud.com>
2026-06-22 00:21:37 +0600

    add cookies

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
-import uvicorn
-from fastapi import FastAPI, staticfiles
+import uvicorn, os
+from fastapi import FastAPI, Cookie
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+from datetime import datetime
 from app.routers.users import users_router
     
 app = FastAPI()
 app.include_router(users_router, tags=["Users"])
-app.mount("/", staticfiles.StaticFiles(directory="app/public", html=True), name="/static")
+
+PATH = "app/public/index.html"
+
+if os.path.exists("public"):
+    app.mount("/public", StaticFiles(directory="public"), name="public")
+
+@app.get("/")
+async def root(last_visit: str | None = Cookie(default=None)):
+    if not os.path.exists(PATH):
+        return {"error": f"File {PATH} is not found!"}
+    if last_visit is None:
+        now = datetime.now().isoformat()
+        response = FileResponse(PATH)
+        response.set_cookie(key="last_visit", value=now)
+        return response
+    else:
+        return FileResponse(PATH)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True)
